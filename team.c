5f9208c0aa3ca82e67dff1593c7f99814858f5e4
Ayman Zeine <a.zeine.96@gmail.com>
2017-12-05 19:14:30 -0500

    Initial Commit

---
+++ team.c
@@ -0,0 +1,12 @@
+#include "support.h"
+
+/*
+ * Please be sure to fill in this struct right away!
+ */
+struct team_t team =
+{
+  "Ayman Zeine", /* first member name, e.g., Michael Spear */
+  "ahz318@lehigh.edu", /* first member email, e.g., mfs409@lehigh.edu */
+  "Eric Picard", /* second member name, e.g., Michael Spear */
+  "ebp219@lehigh.edu"  /* second member email, e.g., mfs409@lehigh.edu */
+};
