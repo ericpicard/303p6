3a0cac97aaa765311c2d9eb7192ffd9b76a9c064
ericpicard <34289283+ericpicard@users.noreply.github.com>
2017-12-09 20:29:37 -0500

    Add files via upload

--- Client.cpp
+++ Client.cpp
@@ -19,8 +19,12 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string>       // std::string
+#include <iostream>     // std::cout
+#include <sstream>
 #include "support.h"
 #include "Client.h"
+using namespace std;
 void sendData(int fd,char * strbuf, size_t size);
 void help(char *progname)
 {
@@ -28,10 +32,46 @@ void help(char *progname)
 	printf("Perform a PUT or a GET from a network file server\n");
 	printf("  -P    PUT file indicated by parameter\n");
 	printf("  -G    GET file indicated by parameter\n");
+	printf("  -E    Use RSA encryption to protect files on server\n");
 	printf("  -C    use checksums for both PUT and GET\n");
 	printf("  -s    server info (IP or hostname)\n");
 	printf("  -p    port on which to contact server\n");
 	printf("  -S    for GETs, name to use when saving file locally\n");
+}
+int padding = RSA_PKCS1_PADDING;
+RSA * createRSAWithFilename(char * filename,int is_public){
+  FILE * fp = fopen(filename,"rb");
+  if(fp == NULL){
+    printf("Unable to open file %s \n",filename);
+    return NULL;
+  }
+  RSA *rsa= RSA_new();
+  if(is_public){
+    rsa = PEM_read_RSA_PUBKEY(fp, &rsa,NULL, NULL);
+  }
+  else{
+    rsa = PEM_read_RSAPrivateKey(fp, &rsa,NULL, NULL);
+  }
+  return rsa;
+}
+int public_encrypt(unsigned char * data,int data_len, char * key, unsigned char *encrypted)
+{
+  RSA * rsa = createRSAWithFilename(key,1);
+  int result = RSA_public_encrypt(data_len,data,encrypted,rsa,padding);
+  return result;
+}
+int private_decrypt(unsigned char * enc_data,int data_len, char * key, unsigned char *decrypted)
+{
+  RSA * rsa = createRSAWithFilename(key,0);
+  int  result = RSA_private_decrypt(data_len,enc_data,decrypted,rsa,padding);
+  return result;
+}
+void printLastError(char *msg){
+  char * err = (char*) malloc(130);
+  ERR_load_crypto_strings();
+  ERR_error_string(ERR_get_error(), err);
+  printf("%s ERROR: %s\n",msg, err);
+  free(err);
 }
 
 void die(const char *msg1, const char *msg2)
@@ -41,11 +81,29 @@ void die(const char *msg1, const char *msg2)
 }
 
 
-char * filetomd5 (char * buffer, int length){
-	int n;
-	MD5_CTX c;
-	unsigned char digest[16];
-	char *out = (char *)malloc(33);
+char *md5sum(const char *str, int length) {
+  int n;
+  MD5_CTX c;
+  unsigned char digest[16];
+  char *out = (char*)malloc(33);
+
+  MD5_Init(&c);
+
+  while (length > 0) {
+    if (length > 512) {
+      MD5_Update(&c, str, 512);
+    } else {
+      MD5_Update(&c, str, length);
+    }
+    length -= 512;
+    str += 512;
+  }
+
+  MD5_Final(digest, &c);
+  for (n = 0; n < 16; ++n) {
+    snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+  }
+  return out;
 }
 
 
@@ -181,10 +239,10 @@ void echo_client(int fd)
 }
 
 
-void getData(int connfd){
+char* getData(int connfd){
 	const int MAXLINE = 8192;
-	char      buf[MAXLINE];   /* a place to store text from the client */
-	bzero(buf, MAXLINE);
+	char *      buf = (char*)malloc(sizeof(char) *MAXLINE);   /* a place to store text from the client */
+	//bzero(buf, MAXLINE);
 
 	/* read from socket, recognizing that we may get short counts */
 	char *bufp = buf;              /* current pointer into buffer */
@@ -207,7 +265,7 @@ void getData(int connfd){
 		{
 			//fprintf(stderr, "%s\n", buf );
 			fprintf(stderr, "received EOF\n");
-			return;
+			return buf;
 		}
 		/* update pointer for next bit of reading */
 		bufp += nsofar;
@@ -218,18 +276,31 @@ void getData(int connfd){
 			break;
 		}
 	}
-	printf("%s\n", buf );
 
-return;
+
+	//printf("%s\n", buf );
+
+return buf;
 
 }
 /*
  * put_file() - send a file to the server accessible via the given socket fd
  */
-void put_file(int fd, char *put_name){
+void put_file(int fd, char *put_name, int checksums){
 	const int MAXLINE = 8192;
 	char buf[MAXLINE];
+	char* checksum = (char *)malloc(sizeof(char)*MAXLINE);
+	char* response = (char *)malloc(sizeof(char)*MAXLINE);
+	if(!checksums){
 	strcpy(buf, "PUT ");
+}
+else{
+	strcpy(buf, "PUTC ");
+}
+if(put_name == NULL){
+	fprintf(stderr, "%s\n", "Please provide a file name." );
+	gets(put_name);
+}
 	size_t n = strlen(buf);
 	size_t nremain = n;
 	ssize_t nsofar;
@@ -239,10 +310,7 @@ void put_file(int fd, char *put_name){
 	//sendData(fd, buf, strlen(buf));
 	//bzero(buf, MAXLINE);
 
-	if(put_name == NULL){
-		fprintf(stderr, "%s\n", "Please provide a file name." );
-		gets(put_name);
-	}
+
 	if(access(put_name, F_OK) != -1){
 		///printf("%s", buf );
 		sendData(fd, buf, strlen(buf));
@@ -252,14 +320,33 @@ void put_file(int fd, char *put_name){
     long filelen = ftell(fileptr);
     rewind(fileptr);
 		char * contents;
-		contents = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
+		char * encryptedContents;
+		unsigned char* contentsforRSA = (unsigned char *)malloc((filelen+1)*sizeof(char));
+		contents = (char *)malloc((filelen+1)*sizeof(char));
+		encryptedContents = (char *)malloc((filelen+1)*sizeof(char));// Enough memory for file + \0
 		fread(contents, filelen, 1, fileptr); // Read in the entire file
 		fclose(fileptr); // Close the file
 		//memcpy(contents + filelen, "\n", 1);
+		unsigned char  encrypted[4098]={};
+    unsigned char decrypted[4098]={};
+		char pubkey[MAXLINE];
+		strcpy(pubkey, "public.pem");
+		memcpy(contentsforRSA, contents, filelen);
+    int encrypted_length= public_encrypt(contentsforRSA,filelen,pubkey,encrypted);
 		char fsize[MAXLINE];
+
 		sprintf(fsize, "%ld\n", filelen);
+		printf("%s\n", encrypted );
 		sendData(fd, fsize, strlen(fsize));
     sleep(1);
+		if(checksums){
+			checksum = md5sum(contents, filelen);
+			printf("%s\n",checksum);
+
+			strcat(checksum, "\n");
+			sendData(fd,checksum, 33);
+			sleep(1);
+		}
 		//memcpy(contents + filelen, buf, 1 );
 		sendData(fd, contents, filelen);
 	//printf("%s", contents);
@@ -267,7 +354,8 @@ void put_file(int fd, char *put_name){
 		sleep(1);
 
 		free(contents);
-		getData(fd);
+		response = getData(fd);
+		printf("%s\n", response );
 		//exit(-1);
 
 	} else {
@@ -282,36 +370,75 @@ void put_file(int fd, char *put_name){
  * get_file() - get a file from the server accessible via the given socket
  *              fd, and save it according to the save_name
  */
-void get_file(int fd, char *get_name, char *save_name)
-{
-
-		/* TODO: implement a proper solution, instead of calling the echo() client */
-		//echo_client(fd);
-		const int MAXLINE = 8192;
-		char request[MAXLINE];
-
-		/* Send the get request to server*/
-		sprintf(request,"GET %s\n", get_name);
-		printf("%s\n", get_name);
-		sendData(fd, request, strlen(request));
-		sleep(1);
-		getData(fd);
-		//getData(fd);
-		//getData(fd);
-		FILE *fp = fopen(get_name, "wb");
-		//fprintf(fp, "%s", reply);
-
-		if(save_name){
-
-		rename(get_name,save_name);
+ void get_file(int fd, char *get_name, char *save_name, int checksums)
+ {
+	 int allow_write = 1;
+printf("%d\n", checksums );
+ 		/* TODO: implement a proper solution, instead of calling the echo() client */
+ 		//echo_client(fd);
+ 		const int MAXLINE = 8192;
+ 		char request[MAXLINE];
+		char* response = (char*)malloc(sizeof(char)* MAXLINE);
+		char* bytes = (char*)malloc(sizeof(char)* MAXLINE);
+		char* checksum = (char*)malloc(sizeof(char)* MAXLINE);
+		char* contents = (char*)malloc(sizeof(char)* MAXLINE);
+ 		/* Send the get request to server*/
+		if(checksums == 0){
+ 		sprintf(request,"GET %s\n", get_name);
 	}
-		fclose(fp);
+	else{
+		sprintf(request,"GETC %s\n", get_name);
+	}
+ 		printf("%s\n", get_name);
+ 		sendData(fd, request, strlen(request));
+		printf("%s\n",request );
+ 		sleep(1);
+ 		response = getData(fd);
+		printf("%s\n", response);
+		if(strncmp(response, "OK", 2) == 0){
 
+ 		bytes = getData(fd);
+		if(checksums){
+			checksum = getData(fd);
+			printf("%s\n", checksum);
+		}
+	//	sleep(1);
+ 		contents = getData(fd);
+		printf("%s\n", bytes );
+		printf("%s\n", contents );
+		stringstream str(bytes);
+		int x;
+		str >> x;
+		printf("%d\n", x);
 
-		/* Receive Response*/
-		//exit(0);
-
+if(checksums){
+		printf("%s\n","Comparing checksum values." );
+		printf("%s\n", md5sum(contents, x) );
+		if(memcmp(checksum, md5sum(contents, x), 32 ) == 0){
+			printf("%s\n", "Received valid checksum.");
+		}else{
+			printf("%s\n", "Error. Checksum mismatch.");
+			allow_write = 0;
 }
+}
+if(allow_write){
+ 		if(FILE *fp = fopen(get_name, "wb")){
+		fwrite(contents, sizeof(char), (x), fp);
+ 		//fprintf(fp, "%s", reply);
+
+ 		if(save_name){
+
+ 		rename(get_name,save_name);
+ 	}
+ 		fclose(fp);
+}
+else printf("%s\n", "Error Opening File.");
+}
+}
+ 		/* Receive Response*/
+ 		//exit(0);
+
+ }
 
 /*
  * main() - parse command line, open a socket, transfer a file
@@ -324,6 +451,7 @@ int main(int argc, char **argv)
 	char *put_name = NULL;
 	char *get_name = NULL;
 	int checksums = 0;
+
 	int   port;
 	char *save_name = NULL;
 
@@ -349,26 +477,15 @@ int main(int argc, char **argv)
 
 	/* put or get, as appropriate */
 
-	if(checksums){
-		if(put_name)
-		{
-			put_file(fd, put_name);
-		}
-		else
-		{
-			get_file(fd, get_name, save_name);
-		}
-	} else {
-
 	if(put_name)
 	{
-		put_file(fd, put_name);
+		put_file(fd, put_name, checksums);
 	}
 	else
 	{
-		get_file(fd, get_name, save_name);
+		get_file(fd, get_name, save_name, checksums);
 	}
-}
+
 
 
 
--- Server.cpp
+++ Server.cpp
@@ -30,13 +30,39 @@ void help(char *progname)
 	printf("  -p    port on which to listen for connections\n");
 }
 
+char *md5sum(const char *str, int length) {
+  int n;
+  MD5_CTX c;
+  unsigned char digest[16];
+  char *out = (char*)malloc(33);
+
+  MD5_Init(&c);
+
+  while (length > 0) {
+    if (length > 512) {
+      MD5_Update(&c, str, 512);
+    }
+    else {
+      MD5_Update(&c, str, length);
+    }
+    length -= 512;
+    str += 512;
+  }
+  MD5_Final(digest, &c);
+  for (n = 0; n < 16; ++n) {
+    snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+  }
+//  printf("%s\n",out);
+  return out;
+}
+
 void sendData(char * buf, int fd){
 	//printf("%s\n","send" );
   size_t n = strlen(buf);
   size_t nremain = n;
   size_t nsofar;
   char *bufp = buf;
-  // printf("sending: %s",strbuf);
+  printf("sending: %s",buf);
   while (nremain > 0) {
     if ((nsofar = write(fd, bufp, nremain)) <= 0) {
       if (errno != EINTR) {
@@ -50,20 +76,26 @@ void sendData(char * buf, int fd){
     nremain -= nsofar;
     bufp += nsofar;
   }
+	printf("SENT %s\n", buf );
 }
 
 
-void try_get(char* request, int fd, int size) {
+void try_get(char* request, int fd, int size, int checksums) {
 	//printf("%s\n", );
 	int MAXLINE = 8192;
 	char get_name[MAXLINE];
 	char *result = (char *)malloc(sizeof (char) * MAXLINE);
 	char *numbytes = (char *)malloc(sizeof (char) * MAXLINE);
-
+	char *data = (char *)malloc(sizeof (char) * MAXLINE);
+  char *checksum = (char *)malloc(sizeof(char) * MAXLINE);
 	std::string buff;
 	get_name[size] = '\0';
 	int j = 0;
-	for(int i = 4; i < size-1; i++) {
+	int start_pos = 4;
+	if(checksums){
+		start_pos = 5;
+	}
+	for(int i = start_pos; i < size-1; i++) {
 		char c = request[i];
 		if(c != '\n'){
 		get_name[j] = c;
@@ -80,12 +112,11 @@ fprintf(stderr, "%s", get_name );
 		fseek(f, 0, SEEK_END);
 		long fsize = ftell(f);
 		fseek(f, 0, SEEK_SET);  //same as rewind(f);
-
 		char string[fsize + 1];
 		fread(string, fsize, 1, f);
 		fclose(f);
 
-		string[fsize] = 0;
+		string[fsize] = '\0';
 		//just send it
 		/*
 		Server sends:
@@ -101,14 +132,29 @@ fprintf(stderr, "%s", get_name );
 		strcpy(result, "OK ");
 		memcpy(result + 3, fnameNewLine, strlen(fnameNewLine));
 		sprintf(numbytes, "%d\n", fsize);
-		memcpy(result+ strlen(result), numbytes, strlen(numbytes));
-		memcpy(result+ strlen(result), string, fsize);
+		//memcpy(result+ strlen(result), numbytes, strlen(numbytes));
+		memcpy(data, string, fsize);
+
 
 		//printf("OK\n");
-		//printf("%d bytes\n", fsize);
-	//	printf("%s", string);
+		printf("%d bytes\n", fsize);
+	printf("%s", string);
 	sendData(result, fd);
+	printf("%s\n",result );
 	sleep(1);
+	sendData(numbytes, fd);
+	if(checksums){
+		checksum = md5sum(data, fsize);
+		printf("%s\n", checksum);
+		strcat(checksum, "\n");
+		sleep(1);
+		sendData(checksum, fd);
+
+	}
+	printf("%s\n",numbytes );
+	sleep(1);
+	sendData(data, fd);
+	printf("%s\n",string );
 
 	free(result);
 	free(numbytes);
@@ -120,12 +166,22 @@ fprintf(stderr, "%s", get_name );
 }
 
 
-char* try_put(char* request, int size){
+char* try_put(char* request, int size, int checksums, int sumlen){
 	int loc= 0;
 	int idx =0;
+	int allow_write =0;
 	int data_start = 0;
+	if(checksums == 0){
+		allow_write =1;
+	}
+	else{
+		allow_write = 0;
+	}
 	char * to_return = (char *)malloc(sizeof(char)*1024);
+
 	const int MAXLINE = 8192;
+	char * checksum = (char *)malloc(sizeof(char)*MAXLINE);
+	//int sumlen = 0;
 	char filesize[MAXLINE];
 	char put_name[MAXLINE];
      for(int i= 0; i<size; i++){
@@ -147,6 +203,9 @@ str >> x;
 //printf("%d\n", x);
 idx = 0;
 int name_start = 4;
+if(checksums){
+	name_start = 5;
+}
 for(int i = name_start; i<size; i++){
 	if(request[i]=='\n'){
 		loc = i + 1;
@@ -159,13 +218,30 @@ for(int i = name_start; i<size; i++){
  put_name[idx] = '\0';
 char * contents;
 contents = (char *)malloc((x)*sizeof(char)); // Enough memory for file + \0
-
-memcpy(contents, request+data_start, x);
+if(checksums){
+	sumlen = 33;
+	memcpy(checksum, request+data_start, 32);
+}
+memcpy(contents, request+data_start+sumlen, x);
 //printf("%s\n", put_name);
 //printf("%s\n", contents);
+printf("%s\n",checksum );
+printf("%s\n",contents );
+
+
+if(memcmp(checksum, md5sum(contents, x), 32 ) == 0){
+	printf("%s\n", "Received valid checksum.");
+	allow_write = 1;
+}else{
+	printf("%s\n", "Error. Checksum mismatch.");
+	allow_write = 0;
+}
+
+	FILE *fp;
 if (access(put_name, F_OK) != -1){
 	//printf("%s\n", "file exists... overwrite" );
-	if(FILE *fp = fopen(put_name, "wb")){
+
+	if((fp = fopen(put_name, "wb"))&& allow_write){
 	fwrite(contents, sizeof(char), (x), fp);
 	fclose(fp);
 	printf("File Updated: %s. Contents:-->%s" , put_name, contents);
@@ -177,7 +253,7 @@ else{
 }
 }
 else{
-	if(FILE *fp = fopen(put_name, "wb")){
+	if(fp = fopen(put_name, "wb")){
 	fwrite(contents, sizeof(char), (x), fp);
 	fclose(fp);
 	printf("File Created: %s. Contents:-->%s" , put_name, contents);
@@ -333,6 +409,60 @@ char * getData(int connfd){
 
 
 		}
+		//bytes = MAXLINE - nremain;
+	  printf("server received %d bytes\n", MAXLINE-nremain);
+		return buf;
+
+}
+
+char * getSum(int connfd, int &bytes){
+
+		const int MAXLINE = 8192;
+		char   *   buf =(char*) malloc (sizeof(char)* MAXLINE);   /* a place to store text from the client */
+		//bzero(buf, MAXLINE);
+
+
+		/* read from socket, recognizing that we may get short counts */
+		char *bufp = buf;              /* current pointer into buffer */
+		ssize_t nremain = MAXLINE;     /* max characters we can still read */
+		size_t nsofar;                 /* characters read so far */
+		while (1)
+		{
+		//	fprintf(stderr, "%s\n", "stuck" );
+			/* read some data; swallow EINTRs */
+			if((nsofar = read(connfd, bufp, nremain)) < 0)
+			{
+				printf("%s\n","error" );
+				if(errno != EINTR)
+				{
+					die("read error: ", strerror(errno));
+				}
+				continue;
+			}
+			/* end service to this client on EOF */
+			if(nsofar == 0)
+			{
+
+				//fprintf(stderr, "%s\n", buf );
+				fprintf(stderr, "received EOF\n");
+				return buf;
+				//die("fdsjkhl", "shjf");
+
+			}
+
+						bufp += nsofar;
+						nremain -= nsofar;
+
+			if(*(bufp-1) == '\n'){
+				*bufp = 0;
+				break;
+				//break;
+			}
+			/* update pointer for next bit of reading */
+
+
+		}
+		bytes = MAXLINE - nremain;
 	  printf("server received %d bytes\n", MAXLINE-nremain);
 		return buf;
 
@@ -354,6 +484,7 @@ void file_server(int connfd, int lru_size){
 		 const int MAXLINE = 8192;
 		 char* filename = (char *)malloc(sizeof(char)* MAXLINE);
 	 	char* fsize = (char *)malloc(sizeof(char)* MAXLINE);
+		char* checksum = (char *)malloc(sizeof(char) * MAXLINE);
 
 		 char      buf[MAXLINE];   /* a place to store text from the client */
 		 bzero(buf, MAXLINE);
@@ -393,14 +524,27 @@ void file_server(int connfd, int lru_size){
 		// bufp = buf;
 		//printf("%s",bufp);
 		/* dump content back to client (again, must handle short counts) */
-
+		int hasChecksum = 0;
+		int sumbytes= 0;
+		int checksumlen = 33;
+		int name_start = 4;
 		//fprintf(stderr,  buf );
+		if(strncmp(buf, "PUTC", 4) == 0){
+			hasChecksum = 1;
+			name_start = 5;
+
+		}
 		if(strncmp(buf, "PUT", 3) ==0){
 			//fprintf(stderr, "This is a put.\n" );
-			memcpy(filename, buf + 4, strlen(buf) - 4);
+			memcpy(filename, buf + name_start, strlen(buf) - name_start);
 			//printf("%s\n",buf );
 			//printf("%s\n", filename );
 			fsize = getData(connfd);
+
+			if(hasChecksum){
+				checksum = getSum(connfd, sumbytes);
+				printf("%s\n",checksum );
+			}
 			//printf("%s\n", fsize );
 
 			memcpy(buf + strlen(buf), fsize, strlen(fsize));
@@ -413,20 +557,30 @@ void file_server(int connfd, int lru_size){
 			contents = getData(connfd);
 			//getData(connfd);
 		  //printf("%s\n", contents );
-
-			memcpy(buf + strlen(buf), contents, size_as_dec);
+			if(hasChecksum){
+				memcpy(buf + strlen(buf), checksum, sumbytes);
+				printf("%s\n",buf );
+			}
+			memcpy(buf + strlen(buf) , contents, size_as_dec);
+			printf("%s\n", buf);
 			//printf("%s\n", buf);
-			result = try_put(buf, MAXLINE - nremain);
+			result = try_put(buf, MAXLINE - nremain, hasChecksum, sumbytes);
 			sleep(1);
 			sendData(result, connfd);
 			//sleep(1);
 
 
 		}
-		else if(strncmp(buf, "GET", 3) == 0) {
-		fprintf(stderr, "This is a get.\n");
-		try_get(buf, connfd, MAXLINE-nremain);
+		else if(strncmp(buf, "GETC", 4) == 0) {
+		fprintf(stderr, "This is a getc.\n");
+		try_get(buf, connfd, MAXLINE-nremain, 1);
 	}
+		else if(strncmp(buf, "GET", 3) == 0) {
+			printf("%s\n",buf );
+		fprintf(stderr, "This is a get.\n");
+		try_get(buf, connfd, MAXLINE-nremain, 0);
+	}
+
 
 	}
 }
@@ -459,7 +613,7 @@ int main(int argc, char **argv)
 		switch(opt)
 		{
 		case 'h': help(argv[0]); break;
-		case 'l': lru_size = atoi(argv[0]); break;
+		case 'l': lru_size = atoi(optarg); break;
 		case 'm': multithread = true;	break;
 		case 'p': port = atoi(optarg); break;
 		}
